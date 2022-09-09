c5f441c13fc6a8157d42ee10b59ae1fcba1f2c40
Vysakh-T <53861783+Vysakh-T@users.noreply.github.com>
2022-09-09 18:53:49 +0530

    initial script

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const getNewAdvice = () => {
-    console.log("New Advice")
+const getNewAdvice = async() => {
+
+    // debounce
+    const request = await fetch("https://api.adviceslip.com/advice");
+    const data = await request.json();
+    updateAdvice(data.slip.id, data.slip.advice)
+}
+
+const updateAdvice = (id, advice) => {
+    slipId = document.getElementById("slip_id")
+    slipAdvice = document.getElementById("slip_advice")
+
+    slipId.innerHTML = id;
+    slipAdvice.innerHTML = advice;
 }
